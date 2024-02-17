5d491c1b980f9b92e990d4993a1fab39060a0303
Philip Pearce-Pearson <pearce_p1@red16.cs.denison.edu>
2024-02-17 17:21:10 -0500

    edited H file

--- minqueue.h
+++ minqueue.h
@@ -1,8 +1,15 @@
+//==============================================================
+// Ryan Daawson, Philip Perry Pearce-Pearson and Aniket Gauba
+// 19th January 2023
+// minqueue.cpp
+// This file contains the MinQueue class declaration.  
+//==============================================================
+
 #ifndef MINQUEUE_H
 #define MINQUEUE_H
-
+#include <sstream>
 #include <string>
-
+using namespace std;
 template <typename T>
 class MinQueue
 {
@@ -21,7 +28,7 @@ private:
         delete[] heapArray;
         heapArray = newHeap;
         capacity = newCapacity;
-    }
+    };
 
     void swap(T &a, T &b) // helper function for swapping two values
     {
@@ -36,15 +43,27 @@ public:
     ~MinQueue();                                      // Destructor - frees up memory allocated by the data class
     MinQueue<T> operator=(const MinQueue<T> &myHeap); // Assignment operator
     void insert(const T &x);                          // Inserts item x into the queue. If it is full, returns error.
-    T min() const;                                    // Returns smallest item in the queue. If it is empty, returns error.
-    T extract_min();                                  // Removes smallest item from the queue and returns it. If it is empty, returns
+    T& min() const;                                    // Returns smallest item in the queue. If it is empty, returns error.
+    T& extract_min();                                  // Removes smallest item from the queue and returns it. If it is empty, returns
     void decrease_key(int i, const T &k);             // Decreases key value of node at index i to k. If it is not present or
-    void heapify(int i);                              // Ensure the subtree rooted at index i follows the heap property
-    void build_min_heap();                            // Builds a min heap from the contents of the array.
-    void heapsort(T *A);                              // Sorts the array using Heapsort algorithm
+    void min_heapify(int i);                              // Ensure the subtree rooted at index i follows the heap property
+    void build_heap();                            // Builds a min heap from the contents of the array.
+    void sort(T *A);                              // Sorts the array using Heapsort algorithm
     string to_string() const;                         // Return a string representation of the queue
     void set(int i, const T &val);                    // Set element at position i to value val
     void allocate(int n);                             // Allocates space for n elements.
+    
+    friend ostream & operator << ( ostream &os, const MinQueue<T> &l )
+    {
+        os << "[ ";
+        for ( int i = 0; i < l.heapsize-1; i++ )
+            os << l.heapArray[i] << ", ";
+        if ( l.heapsize != 0 )
+            os << l.heapArray[l.heapsize-1] << " ]";
+        else
+            os << " ]";
+        return os;
+    }
 };
 
 #endif /* MINQUEUE_H */
